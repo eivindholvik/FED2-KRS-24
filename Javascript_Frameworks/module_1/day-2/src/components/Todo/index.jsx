3c3adbb96a7ace1ec65e42f6ddd845dc621cd2d2
eivindholvik <eivind.holvik@noroff.no>
2024-02-27 11:40:09 +0100

    useReducer

--- Javascript_Frameworks/module_1/day-2/src/components/Todo/index.jsx
+++ Javascript_Frameworks/module_1/day-2/src/components/Todo/index.jsx
@@ -1,10 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
 function Todo({ name = "Todo List" }) {
+    const initialState = { count: 0 };
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "increment":
+                return { count: state.count + 1 };
+            case "decrement":
+                return { count: state.count - 1 };
+            case "set":
+                return { count: action.payload };
+            default:
+                throw new Error();
+        }
+    };
     const [todos, setTodos] = useState();
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // const [counter, setCounter] = useState(0);
+
+    // function increaseCount() {
+    //     setCounter(counter + 1);
+    // }
+
+    // function decreaseCount() {
+    //     setCounter(counter - 1);
+    // }
+
+    // function setIncrementer(value) {
+    //     setCounter(value);
+    // }
 
     useEffect(() => {
         const data = localStorage.getItem(`todos${name}`);
@@ -21,8 +50,43 @@ function Todo({ name = "Todo List" }) {
         }
     }, [todos]);
 
+    const doThis = () => {
+        console.log("Nå ble jeg trigget");
+    };
+
+    // const dependencyArray = [counter, todos];
+
+    // useEffect(doThis, dependencyArray);
+
     return (
         <div className="todoList">
+            <button
+                onClick={() => {
+                    dispatch({ type: "decrement" });
+                }}
+            >
+                -
+            </button>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    dispatch({
+                        type: "set",
+                        payload: Number(e.target.elements.count.value),
+                    });
+                }}
+            >
+                <input name="count" />
+                <input type="submit" value="Set" />
+            </form>
+            {state.count}
+            <button
+                onClick={() => {
+                    dispatch({ type: "increment" });
+                }}
+            >
+                +
+            </button>
             <h2>{name}</h2>
             <TodoForm name={name} todos={todos} setTodos={setTodos} />
             <TodoList todos={todos} setTodos={setTodos} />
